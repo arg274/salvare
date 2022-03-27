b002ab8aa387d6d49f8b1056a082888734064479
arg274 <4648027+arg274@users.noreply.github.com>
2022-03-27 21:50:22 +0600

    Fix crash

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,12 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text(widget.title),
-    ));
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(),
+        ));
   }
 }
